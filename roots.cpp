2cfc60e29d655f7e9c581b2fa361149a9ba4490b
Mateusz Staszczyk <kwiatkiwoknie@gmail.com>
2015-04-01 15:33:09 +0200

    Main file added

---
+++ roots.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int r;
+    float n;
+    cout << "Set root:" << endl;
+    cin >> r;
+    cout << endl << "Set root's degree:" << endl;
+    cin >> n;
+
+    float result = 0;
+    if (r==0 && n<=0) {
+        cout << endl << "Undefined result." << result << endl;
+        return 0;
+    } else if (r==0) {
+        result = 0;
+        cout << endl << "Your result is " << result << "." << endl;
+        return 0;
+    } else if (r==1 && n>=0) {
+        result = 1;
+        cout << endl << "Your result is " << result << "." << endl;
+        return 0;
+    } else if (n<0) {
+        r = 1/r;
+        n = -n;
+    }
+
+    result = r;
+    while (n>1) {
+        result *= r;
+        n--;
+    }
+
+    cout << endl << "Your result is " << result << "." << endl;
+    cin.get();
+    return 0;
+}
